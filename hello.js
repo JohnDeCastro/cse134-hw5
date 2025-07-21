da25ab208d07509b322ac4835a40fc1d67b851ae
johnowow <oao_john@yahoo.com>
2025-07-20 23:52:06 -0700

    update css styling. remove lab.html and hello.js

--- hello.js
+++
@@ -1,8 +0,0 @@
-class HelloJohn extends HTMLElement{
-    constructor(){
-        super();
-        console.log("Hello world from <hello-john>!");
-    }
-}
-
-customElements.define("hello-john", HelloJohn);
